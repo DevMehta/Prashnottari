51312f76b90aa57142d297e192c8f4a40a39da0e
Rohan Shah <shah_rohan2001@yahoo.com>
2023-11-02 20:58:42 +0530

    add quiz and question .py

--- pranshnottriApp/quiz.py
+++ pranshnottriApp/quiz.py
@@ -4,26 +4,70 @@
 	This file represents a class called as quiz.
 '''
 
+from datetime import datetime
+
 class quiz:
 	def __init__(self, ):
 		# instance attributes	
 		self._quiz_id
 		self._quiz_name
-		self._category
-		self.quiz_creation_date
+		self._quiz_category
+		self._quiz_creation_date
 		self._number_of_times_played
-		self._question_ans_dict
 
 	@property
-	def room_id(self):
-		"""The room_id property uniquely identifies a room."""
-		return self._room_id
-	@room_id.setter
-	def room_id(self, room_id_inpt):
+	def quiz_id(self):
+		"""The quiz_id property uniquely identifies a quiz."""
+		return self._quiz_id
+	@quiz_id.setter
+	def quiz_id(self, quiz_id_inpt):
 		try:
-			self._room_id = str(room_id_inpt)
+			self._quiz_id = str(quiz_id_inpt)
 		except ValueError:
-			raise ValueError("room_id has to be a string.")
-		if len(room_id_inpt) != 10:
-			raise ValueError("room_id has to be a string of length 10")
-		self._room_id = room_id
+			raise ValueError("quiz_id has to be a string.")
+		self._quiz_id = quiz_id_inpt
+	
+	@property
+	def quiz_name(self):
+		return self._quiz_name
+	@quiz_name.setter
+	def quiz_name(self, quiz_name_inpt):
+		try:
+			self._quiz_name = str(quiz_name_inpt)
+		except ValueError:
+			raise ValueError("quiz_name has to be a string.")
+		self._quiz_name = quiz_name_inpt
+
+	@property
+	def quiz_category(self):
+		return self._quiz_category
+	@quiz_category.setter
+	def quiz_category(self, quiz_category_inpt):
+		try:
+			self._quiz_category = str(quiz_category_inpt)
+		except ValueError:
+			raise ValueError("quiz_category has to be of datetime type.")
+		self._quiz_category = quiz_category_inpt
+	
+	@property
+	def quiz_creation_date(self):
+		return self._quiz_creation_date
+	@quiz_creation_date.setter
+	def quiz_creation_date(self, quiz_creation_date_inpt):
+		try:
+			self._quiz_creation_date = datetime(quiz_creation_date_inpt)
+		except ValueError:
+			raise ValueError("quiz_creation_date has to be of datetime type.")
+		self._quiz_creation_date = quiz_creation_date_inpt
+			
+
+		@property
+	def number_of_times_played(self):
+		return self._number_of_times_played
+	@number_of_times_played.setter
+	def number_of_times_played(self, number_of_times_played_inpt):
+		try:
+			self._number_of_times_played = str(number_of_times_played_inpt)
+		except ValueError:
+			raise ValueError("number_of_times_played has to be a integer.")
+		self._number_of_times_played = number_of_times_played_inpt
